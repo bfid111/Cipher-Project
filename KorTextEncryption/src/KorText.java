ea2bcae01acadeeb4e01f7f6eba99ecea739e4d7
bfid111 <bfid111@gmail.com>
2016-02-13 14:26:12 -0500

    Test Success

--- KorTextEncryption/src/KorText.java
+++ KorTextEncryption/src/KorText.java
@@ -23,8 +23,24 @@
 public class KorText {
 	
 	public static void main(String[] args){
-		KorTextConverter sample = new KorTextConverter("hello123");
+		KorTextConverter sample = new KorTextConverter("     asdfadsfadsf12334123132!@#!@#!@zxcvcxvzxcvcxzvxzcv   ");
 		sample.convert();
-		System.out.println(sample.returnConverted());   
+                String temp = "";
+		String a = sample.returnConverted();
+                String[] b = splitStringEvery(a,5);
+                System.out.println(a);
+              
 	}
+        public static String[] splitStringEvery(String s, int interval) {
+            int arrayLength = (int) Math.ceil(((s.length() / (double)interval)));
+            String[] result = new String[s.length()/5];
+            int j = 0;
+            int lastIndex = result.length - 1;
+            for (int i = 0; i < lastIndex; i++) {
+                result[i] = s.substring(j, j + interval);
+                j += interval;
+            } //Add the last bit
+            result[lastIndex] = s.substring(j);
+            return result;
+        }
 }
