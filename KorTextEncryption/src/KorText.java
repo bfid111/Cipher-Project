e48be0105648d0fc58bbb0a5d6b7c9c205cc2242
bfid111 <bfid111@gmail.com>
2016-02-13 22:12:13 -0500

    Phase 2 Test done

--- KorTextEncryption/src/KorText.java
+++ KorTextEncryption/src/KorText.java
@@ -23,13 +23,33 @@
 public class KorText {
 	
 	public static void main(String[] args){
-		KorTextConverter sample = new KorTextConverter("This is a test String Number 36 #### 365 asdfljkasdlfladskj");
-                // output= 126131263112625125961203012625125961203012609120301261312599125961261312030125961261312593126251263612622
+		KorTextConverter sample = new KorTextConverter("SooooooooasfdsaaxvzcvxzadsFFsadfAFASFasdfSDAFSdfADFSGDFSAGADSGFADSFADSFADSFASDooo");
+                // output= 
+                //1261312631126251259682030126251259682030126098203012613125991259612613820301259612613125931262512636126228203012636126291264112640125991259382030520348203782030820048200482004820048203052034820372203682030126091259612615126011264312627126231260912596126151264312601126431260912615125961262312627
 		sample.convert();
                 String temp = "";
 		String a = sample.returnConverted();
                 String[] b = splitStringEvery(a,5);
-                System.out.println(a);
+                System.out.println("Original Text : " + "Soo");
+                System.out.println("Converted : " + a);
+                String keyText = "key";
+                int keySum = 0;
+                 for(int i = 0; i < keyText.length(); i++){
+                     keySum += (int)keyText.charAt(i);
+                }
+                System.out.println("Sum = " + keySum);
+                CipherHandler cipher = new CipherHandler(a,"keywithlongcharat");
+                cipher.encrypt();
+                String en = cipher.returnEncrypted();
+                System.out.println("Encrypted = " + en);
+                b = splitStringEvery( cipher.returnEncrypted(),5);
+                int bsum = 0;
+                CipherHandler decipher = new CipherHandler(en,"keywithlongcharat");
+                decipher.decrypt();
+                System.out.println("Decrypted = " + decipher.returnDecrypted());
+                boolean he = a.equals(decipher.returnDecrypted());
+                System.out.println(he);
+                
               
 	}
         public static String[] splitStringEvery(String s, int interval) {
