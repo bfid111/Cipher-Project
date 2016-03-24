7d827e9af4032725b4d371c74707cea95258eea2
bfid111 <bfid111@gmail.com>
2016-03-23 20:01:54 -0400

    Added Decipher

--- KorTextEncryption/src/CipherHandler.java
+++ KorTextEncryption/src/CipherHandler.java
@@ -37,6 +37,7 @@ public class CipherHandler {
             System.out.println(keySum);
             CipherHandler.splitted = stringSplitter(plainText,5);
             for(int a = 0; a < splitted.length; a++){
+                //int tempConverted = Integer.valueOf(splitted[a]) + (keySum);
                 int tempConverted = Integer.valueOf(splitted[a]) + (keySum);
                 System.out.print( splitted[a]);
                 splitted[a] = Integer.toString(tempConverted);
@@ -54,13 +55,27 @@ public class CipherHandler {
             System.out.println(keySum);
             CipherHandler.splitted = stringSplitter(plainText,5);
             for(int a = 0; a < splitted.length; a++){
-                int tempConverted = Integer.valueOf(splitted[a]) - (keySum); // shift decrypt 
+               // int tempConverted = Integer.valueOf(splitted[a]) - (keySum); // shift decrypt 
+                int tempConverted = Integer.valueOf(splitted[a]) + (-keySum);
              //   System.out.println( splitted[a]);
                 splitted[a] = Integer.toString(tempConverted);
             }
             for (String splitted1 : splitted) {
                 decryptText += splitted1;
             }
+            String splittedDeText[] = stringSplitter(decryptText, 5);
+            for(int i = 0; i < splittedDeText.length; i++){
+                System.out.println(splittedDeText[i]);
+            }
+            KorTextConverter dec = new KorTextConverter(decryptText);
+            String decryptFinal = "";
+            for(int i = 0; i < splittedDeText.length; i++){
+                decryptFinal += dec.devert(splittedDeText[i]);
+            }
+            decryptText = decryptFinal;
+            if("".equals(decryptText)){
+                decryptText = "Wrong key! or Cannot be decrypted with this key!";
+            }   
         }
         public static void changeKey(String str){
             keyText = str;
